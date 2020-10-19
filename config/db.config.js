00c8cd1208687163e60dedd81868853239d47f67
dishant1997 <shahdishant28@gmail.com>
2020-10-19 21:08:38 +0530

    changed dbconfig

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,7 @@
 
 require('dotenv').config(); 
-const envirement = process.env.NODE_ENV
+//const envirement = process.env.NODE_ENV
  
-switch (envirement) {  
-    case 'production': 
     database  = {
         HOST:"aa1v63mkw7p9bix.ckscc7hap6ar.eu-central-1.rds.amazonaws.com",
         USER:"AARCHIK",
@@ -19,23 +17,6 @@ switch (envirement) {
             idele:10000
         }
    };
-    break;
-    case 'testing':
-    database  = {
-        HOST:"localhost",
-        USER:"postgres",
-        PASSWORD:"123456",
-        DB:"cxmo",
-        dialect:"postgres",
-        PORT:"5432",
-        pool:{
-            max:5,
-            min:0,
-            acquire:30000,
-            idele:10000
-        }  
-    };
-    break;
- } 
+   
  module.exports = database;
 
