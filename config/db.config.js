f9b7057c8e87d31b5950ce9548a53a9d2af7e032
dishant1997 <shahdishant28@gmail.com>
2020-10-19 20:04:17 +0530

    Added a Procfile.

--- config/db.config.js
+++ config/db.config.js
@@ -5,19 +5,20 @@ const envirement = process.env.NODE_ENV
 switch (envirement) {  
     case 'production': 
     database  = {
-         HOST:"ec2-34-254-24-116.eu-west-1.compute.amazonaws.com",
-         USER:"yazqucvbcqvtsh",
-         PASSWORD:"7bc8276e586b1673e58617d36eda7b0024871b8922c24616e94eba14a29a36a9",
-         DB:"dblfn31njeb5kp",
-         dialect:"postgres",
-         PORT:"5432",
-         pool:{
-             max:5,
-             min:0,
-             acquire:30000,
-             idele:10000
-         }
-    };
+        HOST:"aa1v63mkw7p9bix.ckscc7hap6ar.eu-central-1.rds.amazonaws.com",
+        USER:"AARCHIK",
+        PASSWORD:"063c255764",
+        DB:"cxmo",
+        dialect:"postgres",
+        PORT:"5432",
+        pool:{
+
+            max:5,
+            min:0,
+            acquire:30000,
+            idele:10000
+        }
+   };
     break;
     case 'testing':
     database  = {
